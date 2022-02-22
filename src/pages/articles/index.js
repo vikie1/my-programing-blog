0d11ee8021b05b360daacdf217ef5dcad7612637
vikie1 <39300268+vikie1@users.noreply.github.com>
2022-02-22 14:43:00 +0300

    Styled the blog section

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,3 +1,4 @@
+import { css } from "@emotion/react";
 import { graphql, Link, useStaticQuery } from "gatsby";
 import React from "react";
 import { Header } from "../../components/header/header";
@@ -28,20 +29,107 @@ const ArticlesPage = (props) => {
         return blog.pageContext.id === "" + id;
       }
     });
-    console.log(blogUrl);
     return blogUrl.path;
   };
   return (
     <main>
-      <Header pageTitle="Read Articles" />
-      {data.allBlog.nodes.map((blog) => (
-        <article key={blog.id}>
-          <h2>
-            <Link to={getBlogUrl(blog.id)}>{blog.name}</Link>
-          </h2>
-          <p>{blog.description}</p>
-        </article>
-      ))}
+      <Header pageTitle="Read Blog Articles" />
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <div
+          css={css`
+            width: 85%;
+            display: grid;
+            place-content: center;
+            padding: 3rem 0;
+            @media (min-width: 768px) {
+              grid-template-columns: 1fr 1fr;
+              gap: 2rem;
+            }
+            @media (min-width: 1024px) {
+              grid-template-columns: 1fr 1fr 1fr;
+              gap: 2rem;
+            }
+          `}
+        >
+          {data.allBlog.nodes.map((blog) => (
+            <article
+              key={blog.id}
+              css={css`
+                width: 350px;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                transition: 0.3s;
+                :hover {
+                  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+                }
+              `}
+            >
+              <Link
+                to={getBlogUrl(blog.id)}
+                css={css`
+                  text-decoration: none;
+                  color: black;
+                `}
+              >
+                <span>
+                  <img
+                    src={blog.img}
+                    alt="Blogs preview"
+                    width={348}
+                    height={200}
+                  />
+                </span>
+                <span>
+                  <div>
+                    <h2
+                      css={css`
+                        font-size: larger;
+                        font-family: "Nunito", sans-serif;
+                        margin: 0%;
+                        padding: 0.5rem 1px;
+                      `}
+                    >
+                      {blog.name}
+                    </h2>
+                    <p
+                      css={css`
+                        font-size: medium;
+                        font-family: "Karla", sans-serif;
+                        margin: 0%;
+                      `}
+                    >
+                      {blog.desc}
+                    </p>
+                    <div
+                      css={css`
+                        font-family: "Dosis", sans-serif;
+                        font-weight: bold;
+                        padding: 2px 0;
+                      `}
+                    >
+                      topics:
+                      {blog.topics.split(",").map((topic, index) => (
+                        <span
+                          key={index}
+                          css={css`
+                            padding: 5px;
+                          `}
+                        >
+                          {topic}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                </span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
--- src/pages/articles/{blog.name}.js
+++ src/pages/articles/{blog.name}.js
@@ -1,5 +1,5 @@
+import { css } from "@emotion/react";
 import { graphql } from "gatsby";
-import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { Header } from "../../components/header/header";
 import { compileMDXFunction, useMDXFunction } from "../../lib/mdx";
@@ -7,12 +7,36 @@ import { compileMDXFunction, useMDXFunction } from "../../lib/mdx";
 const BlogPost = ({ data }) => {
   const mdxContents = compileMDXFunction(data.blog.content);
   const MDXContent = useMDXFunction(mdxContents.value);
+  const centerBlogImages = css`
+    aside {
+      display: flex;
+      justify-content: center;
+    }
+  `;
   return (
     <div className="">
-      <Header pageTitle={data.blog.name}/>
-      <h1>{data.blog.name}</h1>
-      <img src={data.blog.img} alt="Blogs hero image" srcset="" loading="lazy" />
-      <MDXContent />
+      <Header pageTitle={data.blog.name} />
+      <div
+        css={[
+          centerBlogImages,
+          css`
+            width: 90%;
+            @media (min-width: 640px) {
+              width: 50%;
+            }
+            margin: 0 auto;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+          `,
+        ]}
+      >
+        <h1>{data.blog.name}</h1>
+        <aside>
+          <img src={data.blog.img} alt="Blogs hero" loading="lazy" />
+        </aside>
+        <MDXContent />
+      </div>
     </div>
   );
 };
