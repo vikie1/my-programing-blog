57aecb4d8bbbd7843d4eb0dcff7f128d233b077d
vikie1 <39300268+vikie1@users.noreply.github.com>
2022-03-17 15:17:05 +0300

    added course page to contain chapters list

--- src/pages/courses/{course.name}.js
+++ src/pages/courses/{course.name}.js
@@ -1,29 +1,35 @@
-import { graphql } from "gatsby";
+import { css } from "@emotion/react";
+import { graphql, Link } from "gatsby";
 import React from "react";
+import { Header } from "../../components/header/header";
 
-export const Course = ({data}) => {
+const Course = ({ data }) => {
     return (
        <div className="-view">
-
+         <Header pageTitle={data.course.name} />
+         <div>
+         <h1>{data.course.name}</h1>
+         <p>{data.course.desc}</p>
+         </div>
+         {data.course.chapters.map(course => (
+           <Link to={"/"} key={course.id}>
+           <ul>
+             <li>{course.chapter}</li>
+           </ul>
+           </Link>
+         ))}
        </div>
     );
 }
+export default Course;
 export const query = graphql`
   query ($id: String) {
     course(id: { eq: $id }) {
-      id
-      img
+      desc
       name
       chapters {
-          content
-      }
-    }
-    allCourse {
-      nodes {
-        desc
-        img
+        chapter
         id
-        name
       }
     }
     allSitePage {
@@ -34,3 +40,9 @@ export const query = graphql`
     }
   }
 `;
+
+/* 
+notes: 
+ - we need a chapter name(or something like that)
+ - Need topics to move up to course or add topic to course
+*/
