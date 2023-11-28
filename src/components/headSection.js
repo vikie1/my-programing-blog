7e15a2e0ae20ac11b9609263f743159a119632b8
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2023-11-28 14:10:00 +0300

    gsdjhsddjh

--- src/components/headSection.js
+++ src/components/headSection.js
@@ -20,73 +20,72 @@ export const Head = ({
       }
     }
   `);
-  const logo = "https://res.cloudinary.com/victor-portfolio/image/upload/v1649851633/gatsby-icon_afracv.png"
+  const logo =
+    "https://res.cloudinary.com/victor-portfolio/image/upload/v1649851633/gatsby-icon_afracv.png";
   return (
     <>
-      <Helmet>
-        <title>
-          {pageTitle} | {siteMetadata.site.siteMetadata.title}
-        </title>
-        <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content={
-            description
-              ? description
-              : "This is a website built by Victor Mwangi focusing on software development"
-          }
-        />
-        <link
-          rel="canonical"
-          href={siteMetadata.site.siteMetadata.siteUrl + siteLocation}
-        />
-        <meta property="og:type" content={pageType ? pageType : "website"} />
-        <meta
-          property="og:url"
-          content={siteMetadata.site.siteMetadata.siteUrl}
-        />
-        <meta
-          property="og:title"
-          content={pageTitle + " | " + siteMetadata.site.siteMetadata.title}
-        />
-        <meta
-          property="og:description"
-          content={
-            description
-              ? description
-              : "This is a website built by Victor Mwangi Focus on software development"
-          }
-        />
-        <meta property="og:image" content={siteImage ? siteImage : logo} />
-        <meta
-          property="og:site_name"
-          content={
-            siteMetadata.site.siteMetadata.title +
-            " - Equip yourself with skills that will definitely land you a job in tech"
-          }
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@_victormwangi" />
-        <meta
-          name="twitter:title"
-          content={pageTitle + " | " + siteMetadata.site.siteMetadata.title}
-        />
-        <meta
-          name="twitter:description"
-          content={
-            description
-              ? description
-              : "This is a website built by Victor Mwangi Focus on software development"
-          }
-        />
-        <meta name="twitter:image" content={siteImage ? siteImage : logo} />
-        <meta name="twitter:site" content="@_victormwangi" />
-        <meta
-          name="twitter:url"
-          content={siteMetadata.site.siteMetadata.siteUrl + siteLocation}
-        />
-        {/* <Adsense /> */}
-      </Helmet>
+      <title>
+        {pageTitle} | {siteMetadata.site.siteMetadata.title}
+      </title>
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content={
+          description
+            ? description
+            : "This is a website built by Victor Mwangi focusing on software development"
+        }
+      />
+      <link
+        rel="canonical"
+        href={siteMetadata.site.siteMetadata.siteUrl + siteLocation}
+      />
+      <meta property="og:type" content={pageType ? pageType : "website"} />
+      <meta
+        property="og:url"
+        content={siteMetadata.site.siteMetadata.siteUrl}
+      />
+      <meta
+        property="og:title"
+        content={pageTitle + " | " + siteMetadata.site.siteMetadata.title}
+      />
+      <meta
+        property="og:description"
+        content={
+          description
+            ? description
+            : "This is a website built by Victor Mwangi Focus on software development"
+        }
+      />
+      <meta property="og:image" content={siteImage ? siteImage : logo} />
+      <meta
+        property="og:site_name"
+        content={
+          siteMetadata.site.siteMetadata.title +
+          " - Equip yourself with skills that will definitely land you a job in tech"
+        }
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@_victormwangi" />
+      <meta
+        name="twitter:title"
+        content={pageTitle + " | " + siteMetadata.site.siteMetadata.title}
+      />
+      <meta
+        name="twitter:description"
+        content={
+          description
+            ? description
+            : "This is a website built by Victor Mwangi Focus on software development"
+        }
+      />
+      <meta name="twitter:image" content={siteImage ? siteImage : logo} />
+      <meta name="twitter:site" content="@_victormwangi" />
+      <meta
+        name="twitter:url"
+        content={siteMetadata.site.siteMetadata.siteUrl + siteLocation}
+      />
+      {/* <Adsense /> */}
     </>
   );
 };
