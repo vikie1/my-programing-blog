5379341c893eb5eaf3b61b13cb039513e00ab1d3
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2023-12-14 16:53:30 +0300

    add lang attribute in html tag

--- src/components/headSection.js
+++ src/components/headSection.js
@@ -22,7 +22,7 @@ export const Head = ({
     "https://res.cloudinary.com/victor-portfolio/image/upload/v1649851633/gatsby-icon_afracv.png";
   return (
     <>
-      <html lang="en" />
+      {/* <html lang="en" /> */}
       <title>
         {pageTitle} | {siteMetadata.site.siteMetadata.title}
       </title>
